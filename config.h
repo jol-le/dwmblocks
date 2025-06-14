f9b0fe67b8c8184e01ac58fa984c68d6421eb1ba
Jona Ohme <ohmejona@gmail.com>
2025-06-15 01:10:39 +0200

    Remove and Fix dwmblock scripts

--- config.h
+++ config.h
@@ -1,8 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"",	"sb-disk",	10,	22},
 	{"",	"sb-memory",	10,	14},
 	{"",	"sb-cpu",	10,	18},
+	{"",	"sb-cpubars",	10,	20},
 	{"",	"sb-nettraf",	1,	16},
 	{"",	"sb-volume",	0,	10},
 	{"",	"sb-battery",	5,	3},
